70f5d7f63980d0515ccc2355e1f3c761c11f4563
purin52002 <purin7635@gmail.com>
2018-07-12 22:02:11 +0900

    copy code

--- python/humanFeedback/humanFeedBackApp/views.py
+++ python/humanFeedback/humanFeedBackApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http.response import HttpResponse
+ 
+def index_page(request, id):
+    return HttpResponse('This is urls test. id = ' + str(id))
