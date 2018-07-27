506b3416b112d9df178ebcec01cf8c681e6845e9
purin52002 <purin7635@gmail.com>
2018-07-27 19:10:42 +0900

    divert rl-teacher

--- python/humanFeedback/humanFeedBackApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HumanfeedbackappConfig(AppConfig):
-    name = 'humanFeedBackApp'
--- python/humanFeedback/humanFeedBackApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
- 
-urlpatterns = [
-    path('<int:id>', views.index_page, name='index_page'),
-    path('', views.my_page, name='my_page'),
-]
